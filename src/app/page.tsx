7ee67dbacbc38ef68bba673d4a2b3a9b867a4330
sanjeevpatel3007 <sanjeevpatel18921@gmail.com>
2026-01-29 07:16:04 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+     <h1 className="text-4xl font-bold underline">Hello World</h1>
+    </div>
+  );
+}
